c160885e1a76383ad9154d77bf8f8dca87163959
emowtion <46008834+emowtion@users.noreply.github.com>
2020-09-26 19:41:04 +0200

    added options to the URL

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Global variables
 
 let employees = [];
-const urlAPI = 'https://randomuser.me/api/?results=12';
+const urlAPI = 'https://randomuser.me/api/?results=12&inc=name, picture,email,location,phone, dob, &noinfo &nat=US';
 const gridContainer = document.querySelector('.grid-container');
 const overlay = document.querySelector('.overlay');
 const modalContainer = document.querySelector('.modal-content');

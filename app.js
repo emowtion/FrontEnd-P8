05bf73ffb0103f9961d1c8bbfaf5537f8e56df9e
emowtion <46008834+emowtion@users.noreply.github.com>
2020-09-26 15:04:53 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const users = [{
+        name: 'Samir', age: 27
+    },
+    {
+        name: 'Angela', age: 30
+    },
+    {
+        name: 'Beatrice', age: 40
+    }
+];
+
+
+
+console.log(users[0].name)
